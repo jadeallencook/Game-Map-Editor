8d1ab3836031834f87ab947ca156c40d9d7f8030
Clayton Cook <claytoncook@Claytons-MacBook-Pro-2.local>
2019-05-05 14:33:56 -0400

    🛠Worked on Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
+
 
 const Navbar = props => (
     <div className="Navbar">
-        Navbar works!
+        <Link className="clickable" to="/games">Games</Link>
+        <Link className="clickable" to="/game">Overview</Link>
+        <Link className="clickable" to="/maps">Maps</Link>
+        <Link className="clickable" to="/player">Player</Link>
+        <Link className="clickable" to="/enemies">Enemies</Link>
+        <Link className="clickable" to="/objects">Objects</Link>
+        <Link className="clickable" to="/settings">Settings</Link>
     </div>
 );
 
