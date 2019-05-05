941b9b7bd92c1ea5a5fba124bbff686bd80399c4
Jade Allen Cook <jadeallencook@gmail.com>
2019-05-05 18:44:14 -0400

    🎉 Started Maps component and added style classes

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -6,11 +6,69 @@ class Maps extends Component {
         super();
         this.state = {};
     }
+
+    create(event) {
+        event.preventDefault();
+    }
+
+    load(event) {
+        console.log(event)
+    }
+
+    remove() {
+
+    }
+
     render() {
         return (
-        <div className="Maps">
-            Maps works!
-        </div>
+            <div className="Maps">
+                <div>
+                    <select value="none" className="dropdown" onChange={this.load.bind(this)}>
+                        <option value="none" disabled>No Maps (Create A New One)</option>
+                    </select>
+                    <button className="btn danger clickable">Remove</button>
+                    <form onSubmit={this.create.bind(this)}>
+                        <input type="text" placeholder="Map Name" className="input" required />
+                        <input type="number" placeholder="Height" className="input" required />
+                        <input type="number" placeholder="Width" className="input" required />
+                        <input type="submit" value="Create Map" className="btn clickable" required />
+                    </form>
+                </div>
+                <div>
+                    <div>
+                        <h2>Editor</h2>
+                        <select className="dropdown">
+                            <option>Ground</option>
+                            <option>Player</option>
+                            <option>Sky</option>
+                        </select>
+                        <input type="number" placeholder="Height" className="input" required />
+                        <input type="number" placeholder="Width" className="input" required />
+                        <h2>Tiles</h2>
+                        <ul>
+                            <li>Grass</li>
+                            <li>Gravel</li>
+                            <li>Water</li>
+                            <li>Other</li>
+                        </ul>
+                        <ul><li></li></ul>
+                        <h2>Enemies</h2>
+                        <h2>Items</h2>
+                        <h2>Actions</h2>
+                    </div>
+                    <div>
+                        <div>
+                            <span>+</span>
+                            <span>-</span>
+                        </div>
+                        {
+                            (() => {
+                                console.log(true);
+                            })()
+                        }
+                    </div>
+                </div>
+            </div>
         );
     }
 }
