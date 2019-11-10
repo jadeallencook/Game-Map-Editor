e705cce37873882e57cd977abba51868a6c83aa2
Jade Allen Cook <jadeallencook@gmail.com>
2019-11-10 11:08:27 -0800

    🚀 Exported new production build

---
+++ build/precache-manifest.4fb35774a7ad67b4d24b4135e9e480c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c462bd365ef3f14215a",
+    "url": "/Game-Map-Editor/build/static/js/runtime~main.4c462bd3.js"
+  },
+  {
+    "revision": "d985c685618c04776ac6",
+    "url": "/Game-Map-Editor/build/static/js/main.d985c685.chunk.js"
+  },
+  {
+    "revision": "31c6ffec22fe9b6a1610",
+    "url": "/Game-Map-Editor/build/static/js/2.31c6ffec.chunk.js"
+  },
+  {
+    "revision": "d985c685618c04776ac6",
+    "url": "/Game-Map-Editor/build/static/css/main.7aeb623e.chunk.css"
+  },
+  {
+    "revision": "9a536e99f39c918eb7d0f636f943d223",
+    "url": "/Game-Map-Editor/build/index.html"
+  }
+];
